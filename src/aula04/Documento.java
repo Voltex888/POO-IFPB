e3fe0f6b5a7c0b7f48a8b54b8344ff5449a347d7
Matheus <Matheuskkevin021@gmail.com>
2026-03-18 00:11:23 -0300

    Terminando a classe Documento

--- src/aula04/Documento.java
+++ src/aula04/Documento.java
@@ -5,6 +5,38 @@ public class Documento {
     private int numeroPaginas;
     private String prioridade, status;
     public Documento(String nome, int numeroPaginas, String prioridade){
+        if (nome == null || nome.isEmpty() || numeroPaginas <=0 || prioridade == null
+                || !prioridade.equals("NORMAL") && !prioridade.equals("URGENTE")){
+            throw new IllegalArgumentException();
+        }
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+        this.prioridade = prioridade;
+        this.status = "AGUARDANDO";
+    }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void iniciarImpressao() {
+        if (status.equals("AGUARDANDO")){
+            status = "IMPRIMINDO";
+        }
+    }
+
+    public void concluir() {
+        if (status.equals("IMPRIMINDO")){
+            status = "CONCLUIDO";
+        }
+    }
+    public String getPrioridade(){
+        return prioridade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getNumeroPaginas(){
+        return numeroPaginas;
     }
 }
