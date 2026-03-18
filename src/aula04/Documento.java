7de3bf18db7efbe9dff1c95349a024aa1998aa73
Matheus <Matheuskkevin021@gmail.com>
2026-03-17 22:30:13 -0300

    Iniciando atividade de Documentos

---
+++ src/aula04/Documento.java
@@ -0,0 +1,10 @@
+package aula04;
+
+public class Documento {
+    private String nome;
+    private int numeroPaginas;
+    private String prioridade, status;
+    public Documento(String nome, int numeroPaginas, String prioridade){
+
+    }
+}
