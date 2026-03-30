f5a5541d85252bfda26aed56f9d92a2980babd3c
Matheus <Matheuskkevin021@gmail.com>
2026-03-30 18:03:30 -0300

    Adicionando Agendamento em Aula 06

---
+++ src/Aula06/Agendamento.java
@@ -0,0 +1,59 @@
+package Aula06;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Agendamento {
+    Cliente cliente;
+    List<Servico> servicos;
+    Veiculo veiculo;
+
+    public Agendamento(Cliente cliente, Veiculo veiculo) {
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.servicos = new ArrayList<>();
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Servico> getServicos() {
+        return Collections.unmodifiableList(servicos);
+    }
+
+    public void addServico(Servico servico) {
+        servicos.add(servico);
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public double custoTotal() {
+            double valor = 0;
+            for (Servico s : servicos) {
+                valor += s.getSubTotal();
+            }
+            if (veiculo.getClassificacao().equals("PESADO")) return valor * 0.30;
+            return valor;
+        }
+
+    @Override
+    public String toString() {
+        return "Agendamento{" +
+                "\n\tcliente=" + cliente +
+                "\n\tserviços=" + servicos.size() +
+                "\n\tveículo=" + veiculo +
+                "\n}";
+    }
+}
