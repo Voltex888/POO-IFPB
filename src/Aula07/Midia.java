db4fd89f14b889e67e138106fe5837368c4e8656
Matheus <Matheuskkevin021@gmail.com>
2026-03-30 18:08:59 -0300

    Adicionando Midia.java da aula 07

---
+++ src/Aula07/Midia.java
@@ -0,0 +1,53 @@
+package Aula07;
+
+public class Midia {
+    private String tipo;
+    private int duracaoSegundos;
+    private String resolucao;
+
+    public Midia(String tipo, int duracaoSegundos, String resolucao) {
+        this.tipo = tipo;
+        this.duracaoSegundos = duracaoSegundos;
+        this.resolucao = resolucao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDuracaoSegundos() {
+        return duracaoSegundos;
+    }
+
+    public void setDuracaoSegundos(int duracaoSegundos) {
+        this.duracaoSegundos = duracaoSegundos;
+    }
+
+    public String getResolucao() {
+        return resolucao;
+    }
+
+    public void setResolucao(String resolucao) {
+        this.resolucao = resolucao;
+    }
+
+    public String descricao() {
+        if (tipo.equalsIgnoreCase("video")) {
+            return "Vídeo " + resolucao + " | " + duracaoSegundos + "s";
+        }
+
+        if (tipo.equalsIgnoreCase("imagem")) {
+            return "Imagem " + resolucao;
+        }
+
+        if (tipo.equalsIgnoreCase("audio")) {
+            return "Áudio | " + duracaoSegundos + "s";
+        }
+
+        return "Mídia desconhecida";
+    }
+}
