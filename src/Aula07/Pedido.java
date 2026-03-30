9d03b04957693218661acae88082cf6fca773743
Matheus <Matheuskkevin021@gmail.com>
2026-03-30 18:09:26 -0300

    Adicionando Pedido.java da aula 07

---
+++ src/Aula07/Pedido.java
@@ -0,0 +1,38 @@
+package Aula07;
+
+import java.util.Collection;
+
+public class Pedido {
+    private String nome;
+    private String data;
+    private Formula formula;
+
+    public Pedido(String nome, String data, Formula formula) {
+        this.nome = nome;
+        this.data = data;
+        this.formula = formula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String resumo() {
+        String texto = "Paciente: " + nome + "\n";
+        texto += "Entrega: " + data + "\n";
+        texto += formula.gerarReceituario();
+        return texto;
+    }
+}
