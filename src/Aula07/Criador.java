3f25f51546f1c22da1001dba05ac660190776be3
Matheus <Matheuskkevin021@gmail.com>
2026-03-30 18:07:03 -0300

    Adicionando Criador.java da aula 07

---
+++ src/Aula07/Criador.java
@@ -0,0 +1,44 @@
+package Aula07;
+
+import java.util.Collection;
+
+public class Criador {
+    private String nomeUsuario;
+    private int seguidores;
+    private boolean verificado;
+
+    public Criador(String nomeUsuario, int seguidores, boolean verificado) {
+        this.nomeUsuario = nomeUsuario;
+        this.seguidores = seguidores;
+        this.verificado = verificado;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public int getSeguidores() {
+        return seguidores;
+    }
+
+    public void setSeguidores(int seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    public boolean isVerificado() {
+        return verificado;
+    }
+
+    public void setVerificado(boolean verificado) {
+        this.verificado = verificado;
+    }
+
+    public String perfil() {
+        String selo = verificado ? " ✓" : "";
+        return "@" + nomeUsuario + " (" + seguidores + " seguidores)" + selo;
+    }
+}
