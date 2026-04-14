a1053c04cabebb2ca1adac855b0632c5988f6914
Matheus <Matheuskkevin021@gmail.com>
2026-04-13 21:56:49 -0300

    Adicionando Main.java da Aula10

---
+++ src/Aula10/Questao01/Main.java
@@ -0,0 +1,9 @@
+package Aula10.Questao01;
+
+public class Main {
+    static void main(String[] args) {
+        VeiculoCPF v1 = new VeiculoCPF("a1564", "2025", "leve", "101-321-789");
+        System.out.println(v1.descricao());
+
+    }
+}
