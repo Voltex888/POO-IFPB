3a48c70f08751eae3e4be9df112173fec162c1b1
Matheus <Matheuskkevin021@gmail.com>
2026-04-13 21:54:43 -0300

    Adicionando Oficina.java da Aula09

---
+++ src/Aula09/Oficina/Oficina.java
@@ -0,0 +1,33 @@
+package Aula09.Oficina;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Oficina {
+    private List<Agendamento> agendamentos = new ArrayList<>();
+    public void adicionar(Agendamento a) {
+        agendamentos.add(a);
+    }
+
+    public List<Agendamento> getAgendamentos() {
+        return Collections.unmodifiableList(agendamentos);
+    }
+    public double faturamentoTotalDoDia(){
+        double valor = 0;
+        for (Agendamento a : agendamentos){
+            valor += a.custoTotal();
+        }
+        return valor;
+    }
+
+    public void gerarRelatorio() {
+        for (Agendamento a : getAgendamentos()){
+            System.out.printf("""
+                    Cliente: %s
+                    Tipo: %s
+                    Custo: R$ %.2f
+                    """, a.getCliente(), a.getClass().getSimpleName(), a.calcularCustoTotal());
+        }
+    }
+}
