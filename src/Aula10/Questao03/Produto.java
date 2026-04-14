11fd51b14b89406d2cd050a166c8e2da486eb4e3
Matheus <Matheuskkevin021@gmail.com>
2026-04-14 08:58:25 -0300

    Adicionando Produto.java da Aula 10

---
+++ src/Aula10/Questao03/Produto.java
@@ -0,0 +1,43 @@
+package Aula10.Questao03;
+
+import java.util.Objects;
+
+public class Produto {
+    private String titulo, autor;
+    private double preco;
+
+    public Produto(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "\n\ttitulo: '" + titulo +
+                "\n\t, autor: '" + autor +
+                "\n\t, preco: " + preco +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Double.compare(preco, produto.preco) == 0 && Objects.equals(titulo, produto.titulo) && Objects.equals(autor, produto.autor);
+    }
+
+}
