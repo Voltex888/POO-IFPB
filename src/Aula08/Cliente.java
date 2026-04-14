4ddcb545eca78e3ab6d43710f8b6e69b2e4ce6f2
Matheus <Matheuskkevin021@gmail.com>
2026-04-13 21:45:42 -0300

    Adicionando Cliente.java da Aula08

---
+++ src/Aula08/Cliente.java
@@ -0,0 +1,31 @@
+package Aula08;
+
+public class Cliente {
+    private String nomeCliente, numero;
+
+    public Cliente(String nomeCliente, String numero) {
+        this.nomeCliente = nomeCliente;
+        this.numero = numero;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    @Override
+    public String toString(){
+        return nomeCliente + ", Numero: " + numero;
+    }
+}
+
