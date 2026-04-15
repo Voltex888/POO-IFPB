f022cc000d2bc658346de70b5c59fd138dd3d1e3
Matheus <Matheuskkevin021@gmail.com>
2026-04-15 18:29:18 -0300

    Finalizando a PlataformaLearnHub.java da Aula-10

---
+++ src/Aula10/Questao04/PlataformaLearnHub.java
@@ -0,0 +1,65 @@
+package Aula10.Questao04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlataformaLearnHub {
+    private List<Avaliavel> avaliaveis = new ArrayList<>();
+    private List<Compravel> compraveis = new ArrayList<>();
+
+    public void cadastrar(Avaliavel avaliavel) {
+        avaliaveis.add(avaliavel);
+    }
+
+    public List<Compravel> listarCompravel() {
+        List<Compravel> lista = new ArrayList<>();
+        for(Avaliavel a : avaliaveis){
+            if(a instanceof Compravel b){
+                lista.add(b);
+            }
+        }
+        return lista;
+    }
+
+    public List<Avaliavel> listarPorNotaMinima(double notaMinima) {
+        List<Avaliavel> listaNotasMinimas = new ArrayList<>();
+
+        for (Avaliavel a : avaliaveis) {
+            if (a.getNotaMedia() >= notaMinima) {
+                listaNotasMinimas.add(a);
+            }
+        }
+        return listaNotasMinimas;
+    }
+
+    public String gerarPaginaDeBusca(double notaMinina) {
+        StringBuilder res = new StringBuilder();
+        for (Avaliavel a : avaliaveis) {
+            if(a.getNotaMedia() >= notaMinina){
+                res.append(a.gerarResumoPublico() + "\n");
+            }
+        }
+        return res.toString();
+    }
+
+    public String processarCompra(String nomeComprador, String tituloCurso) {
+        for (Avaliavel avaliavel : avaliaveis){
+            if(avaliavel instanceof Curso b){
+                Curso curso = b;
+
+                if(curso.getTitulo().equals(tituloCurso) && curso instanceof Compravel c){
+                    return c.gerarRecibo(nomeComprador);
+                }
+            }
+        }
+        return null;
+    }
+    public List<String> gerarRecibosEmLote(String nomeComprador){
+        List<String> recibos = new ArrayList<>();
+        for (Avaliavel avaliavel : avaliaveis){
+            if(avaliavel instanceof Compravel c)
+            recibos.add(c.gerarRecibo(nomeComprador) + "\n");
+        }
+        return recibos;
+    }
+}
