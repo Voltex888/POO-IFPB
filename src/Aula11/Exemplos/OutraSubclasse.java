714bfccdf5b28938e746e0609ed0d218d10fda09
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 20:32:57 -0300

    Adicionando OutraSubclasse da Aula-11

---
+++ src/Aula11/Exemplos/OutraSubclasse.java
@@ -0,0 +1,13 @@
+package Aula11.Exemplos;
+
+public class OutraSubclasse extends Superclasse{
+
+    public static void imprimir() {
+        System.out.println("Método imprimir da OutraSubclasse");
+    }
+
+
+    public static void herdado() {
+        System.out.println("Método herdado de OutraSubclasse");
+    }
+}
