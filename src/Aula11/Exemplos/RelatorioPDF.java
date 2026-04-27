797f8768ce9d9bdd195abe52342c8d0039ba9640
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 20:33:31 -0300

    Adicionando RelatorioPDF.java da Aula-11

---
+++ src/Aula11/Exemplos/RelatorioPDF.java
@@ -0,0 +1,8 @@
+package Aula11.Exemplos;
+
+public class RelatorioPDF extends Relatorio{
+    @Override
+    public void imprimir() {
+        System.out.println("Imprimindo RelatórioPDF");
+    }
+}
