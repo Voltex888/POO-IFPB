121b47863e80b013e897c033ce8a3fd24d6ae1e7
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 20:16:51 -0300

    Alterando Processo.java da questão 01 em Aula-11

--- src/Aula11/q1/Processo.java
+++ src/Aula11/q1/Processo.java
@@ -1,5 +1,21 @@
 package Aula11.q1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Processo {
-    public void processarPagamento()
+
+    List<Pagamento> pagamentos;
+
+    public Processo(List<Pagamento> pagamentos){
+        this.pagamentos = pagamentos;
+    }
+
+    public void processarPagamentos() {
+
+        for(Pagamento p : pagamentos){
+            p.processarPagamento();
+        }
+
+    }
 }
