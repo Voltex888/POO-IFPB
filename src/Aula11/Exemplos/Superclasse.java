1b9720fb36ff79fe5392a84eb476a37bc193866f
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 20:31:48 -0300

    Adicionando Superclasse da Aula-11

---
+++ src/Aula11/Exemplos/Superclasse.java
@@ -0,0 +1,10 @@
+package Aula11.Exemplos;
+
+public class Superclasse {
+    public static void imprimir(){
+        System.out.println("Método da Superclasse");
+    }
+    public static void herdado (){
+        System.out.println("Método herdado da Superclasse");
+    }
+}
