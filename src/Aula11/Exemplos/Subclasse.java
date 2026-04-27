289b860d7f9b72bd37819c931e896c3a52bd5616
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 20:32:24 -0300

    Adicionando Subclasse da Aula-11

---
+++ src/Aula11/Exemplos/Subclasse.java
@@ -0,0 +1,12 @@
+package Aula11.Exemplos;
+
+public class Subclasse extends Superclasse{
+
+    public static void imprimir() {
+        System.out.println("Método da Subclasse");;
+    }
+
+    public static void herdado() {
+        System.out.println("Método herdado da Subclasse");
+    }
+}
