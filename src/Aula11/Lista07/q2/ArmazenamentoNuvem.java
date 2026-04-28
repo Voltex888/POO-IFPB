b0dca8c4bdf3be9739d12e96d1357106d647cac1
Matheus <Matheuskkevin021@gmail.com>
2026-04-28 09:09:29 -0300

    Alterando ArmazenamentoNuvem.java da Questão-02 em Aula-11

--- src/Aula11/Lista07/q2/ArmazenamentoNuvem.java
+++ src/Aula11/Lista07/q2/ArmazenamentoNuvem.java
@@ -1,4 +1,16 @@
 package Aula11.Lista07.q2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArmazenamentoNuvem {
+    private Map<String, byte[]> storage = new HashMap<>();
+
+    public void gravar(String caminho, byte[] dados) {
+        storage.put(caminho, dados);
+    }
+
+    public byte[] ler(String caminho) {
+        return storage.get(caminho);
+    }
 }
