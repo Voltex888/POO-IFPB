869b40d59c92d7606fd3ea0bd3451aaeef7dd11c
Matheus <Matheuskkevin021@gmail.com>
2026-04-27 21:00:32 -0300

    Alterando DocumentoFiscal.java da Questão 01 em Aula-11

--- src/Aula11/Lista07/q1/DocumentoFiscal.java
+++ src/Aula11/Lista07/q1/DocumentoFiscal.java
@@ -1,4 +1,14 @@
 package Aula11.Lista07.q1;
 
-public class DocumentoFiscal {
+public abstract class DocumentoFiscal {
+    protected double valor;
+    protected String indentificador;
+
+    public DocumentoFiscal(double valor, String indentificador) {
+        this.valor = valor;
+        this.indentificador = indentificador;
+    }
+    public abstract double calcularImposto();
+
+    public abstract String gerarXML();
 }
