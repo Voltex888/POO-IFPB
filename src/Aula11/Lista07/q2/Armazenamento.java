1743c565397e68dae09f7ad3b7c39193c0522da0
Matheus <Matheuskkevin021@gmail.com>
2026-04-28 10:19:14 -0300

    Removendo método realizarBackup em Armazenamento.java, Questão-02 em Aula-11

--- src/Aula11/Lista07/q2/Armazenamento.java
+++ src/Aula11/Lista07/q2/Armazenamento.java
@@ -6,6 +6,4 @@ public interface Armazenamento {
     void gravar(String caminho, byte[] dados);
 
     byte[] ler(String caminho);
-
-    void realizarBackup(Armazenamento origem, Armazenamento destino, List<String> caminhos);
 }
