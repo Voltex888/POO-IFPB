66d0ff7efb16f51ef7d3d4547bc0b16535b58dfa
Matheus <Matheuskkevin021@gmail.com>
2026-04-28 10:11:59 -0300

    Alterando Armazenamento para interface, Questão-02 em Aula-11

--- src/Aula11/Lista07/q2/Armazenamento.java
+++ src/Aula11/Lista07/q2/Armazenamento.java
@@ -2,10 +2,10 @@ package Aula11.Lista07.q2;
 
 import java.util.List;
 
-public abstract class Armazenamento {
-    protected abstract void gravar(String caminho, byte[] dados);
+public interface Armazenamento {
+    void gravar(String caminho, byte[] dados);
 
-    protected abstract byte[] ler(String caminho);
+    byte[] ler(String caminho);
 
-    protected abstract void realizarBackup(Armazenamento origem, Armazenamento destino, List<String> caminhos);
+    void realizarBackup(Armazenamento origem, Armazenamento destino, List<String> caminhos);
 }
