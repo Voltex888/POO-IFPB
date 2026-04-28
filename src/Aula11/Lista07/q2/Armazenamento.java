4f86230556c059af92df8ff4576186e40e6cbfee
Matheus <Matheuskkevin021@gmail.com>
2026-04-28 09:20:52 -0300

    Alterando Armazenamento.java da Questão-02 em Aula-11

--- src/Aula11/Lista07/q2/Armazenamento.java
+++ src/Aula11/Lista07/q2/Armazenamento.java
@@ -1,4 +1,11 @@
 package Aula11.Lista07.q2;
 
-public class Armazenamento {
+import java.util.List;
+
+public abstract class Armazenamento {
+    protected abstract void gravar(String caminho, byte[] dados);
+
+    protected abstract byte[] ler(String caminho);
+
+    protected abstract void realizarBackup(Armazenamento origem, Armazenamento destino, List<String> caminhos);
 }
