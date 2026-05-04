b0bea4d8eab9457e3b70ef5221c09b4aeb027889
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 13:58:16 -0300

    Adicionando ProcessadorFiscal.java de q1, aula-11

--- src/Aula11/Lista07/q1/ProcessadorFiscal.java
+++ src/Aula11/Lista07/q1/ProcessadorFiscal.java
@@ -2,8 +2,8 @@ package Aula11.Lista07.q1;
 
 public class ProcessadorFiscal {
 
-    public String processar(DocumentoFiscal doc) {
-        double imposto = doc.calcularImposto();
+    public String processar(DocumentoFiscal doc, double valor) {
+        double imposto = doc.calcularImposto(valor);
 
         String xml = doc.gerarXML();
         return xml + "|imposto:" + String.format("%.2f", imposto);
