ee63193c10af4d3b6af8a9616cfecd92fa8f9129
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 14:49:16 -0300

    Alterando Item.java da Questão-02 em Lista-07, Aula-11

--- src/Aula11/Lista07/q3/Item.java
+++ src/Aula11/Lista07/q3/Item.java
@@ -1,7 +1,13 @@
 package Aula11.Lista07.q3;
 
-public class Itens {
+public class Item {
     private String nome;
     private Double valor;
-    
+    private int quantidade;
+
+    public Item(String nome, Double valor, int quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
 }
