1847ac6f60cf0dd66de3d7857211e1236d8706c8
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 13:57:36 -0300

    Adicionando NFe.java de q1, aula-11

--- src/Aula11/Lista07/q1/NFe.java
+++ src/Aula11/Lista07/q1/NFe.java
@@ -1,17 +1,25 @@
 package Aula11.Lista07.q1;
 
-public class NFe extends DocumentoFiscal{
+public class NFe implements DocumentoFiscal{
+    double valor;
+    String indentificador;
     public NFe(double valor, String indentificador) {
-        super(valor, indentificador);
+        this.valor = valor;
+        this.indentificador = indentificador;
     }
 
     @Override
-    public double calcularImposto() {
+    public double calcularImposto(double valor) {
         return valor * 0.18;
     }
 
     @Override
     public String gerarXML() {
         return "<nfe>" + indentificador + "</nfe>";
+    }
+
+    @Override
+    public String getIndentificador() {
+        return indentificador;
     }
 }
