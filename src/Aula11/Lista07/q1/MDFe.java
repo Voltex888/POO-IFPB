467ffeb6cdeeab24bfaa349aa5b2013b7784b9aa
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 13:57:23 -0300

    Adicionando MDFe.java de q1, aula-11

--- src/Aula11/Lista07/q1/MDFe.java
+++ src/Aula11/Lista07/q1/MDFe.java
@@ -1,17 +1,25 @@
 package Aula11.Lista07.q1;
 
-public class MDFe extends DocumentoFiscal{
+public class MDFe implements DocumentoFiscal{
+    double valor;
+    String indentificador;
     public MDFe(double valor, String indentificador) {
-        super(valor, indentificador);
+        this.valor = valor;
+        this.indentificador = indentificador;
     }
 
     @Override
-    public double calcularImposto() {
+    public double calcularImposto(double valor) {
         return valor * 0.08;
     }
 
     @Override
     public String gerarXML() {
         return "<mdfe>" + indentificador + "</mdfe>";
+    }
+
+    @Override
+    public String getIndentificador() {
+        return indentificador;
     }
 }
