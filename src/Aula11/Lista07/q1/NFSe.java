14d703f9eb74f5d7977c077f183918855a663d0e
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 13:57:56 -0300

    Adicionando NFSe.java de q1, aula-11

--- src/Aula11/Lista07/q1/NFSe.java
+++ src/Aula11/Lista07/q1/NFSe.java
@@ -1,17 +1,25 @@
 package Aula11.Lista07.q1;
 
-public class NFSe extends DocumentoFiscal{
+public class NFSe implements DocumentoFiscal{
+    double valor;
+    String indentificador;
     public NFSe(double valor, String indentificador) {
-        super(valor, indentificador);
+        this.valor = valor;
+        this.indentificador = indentificador;
     }
 
     @Override
-    public double calcularImposto() {
+    public double calcularImposto(double valor) {
         return valor * 0.05;
     }
 
     @Override
     public String gerarXML() {
         return "<nfse>" + indentificador + "</nfse>";
+    }
+
+    @Override
+    public String getIndentificador() {
+        return indentificador;
     }
 }
