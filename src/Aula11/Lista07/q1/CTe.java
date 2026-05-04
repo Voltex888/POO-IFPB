420139c69c0c9a13bc5fb94e33b531492edcacf2
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 13:56:43 -0300

    Adicionando CTe.java de q1, aula-11

--- src/Aula11/Lista07/q1/CTe.java
+++ src/Aula11/Lista07/q1/CTe.java
@@ -1,17 +1,23 @@
 package Aula11.Lista07.q1;
 
-public class CTe extends DocumentoFiscal{
+public class CTe implements DocumentoFiscal{
+    double valor;
+    String indentificador;
     public CTe(double valor, String indentificador) {
-        super(valor, indentificador);
     }
 
     @Override
-    public double calcularImposto() {
+    public double calcularImposto(double valor) {
         return valor * 0.12;
     }
 
     @Override
     public String gerarXML() {
         return "<cte>" + indentificador + "</cte>";
+    }
+
+    @Override
+    public String getIndentificador() {
+        return indentificador;
     }
 }
