d18bf6a07563f6ed89dc63527cb1d5f17ead4f1d
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 14:12:26 -0300

    Alterando ProcessadorFiscal.java da Questão-01 em Lista-07, Aula-11

--- src/Aula11/Lista07/q1/ProcessadorFiscal.java
+++ src/Aula11/Lista07/q1/ProcessadorFiscal.java
@@ -2,8 +2,8 @@ package Aula11.Lista07.q1;
 
 public class ProcessadorFiscal {
 
-    public String processar(DocumentoFiscal doc, double valor) {
-        double imposto = doc.calcularImposto(valor);
+    public String processar(DocumentoFiscal doc) {
+        double imposto = doc.calcularImposto();
 
         String xml = doc.gerarXML();
         return xml + "|imposto:" + String.format("%.2f", imposto);
