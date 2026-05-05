c5fc1615583d330e564c0de24934c67c3f4d693a
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 19:45:06 -0300

    Adicionando, try catch e criando metódos static GeradorDeRelatorio.java da Questão-02, Aula-13

--- src/Aula13/Exercicios/Questao02/GeradorDeRelatorio.java
+++ src/Aula13/Exercicios/Questao02/GeradorDeRelatorio.java
@@ -1,4 +1,23 @@
 package Aula13.Exercicios.Questao02;
 
+import java.io.*;
+
 public class GeradorDeRelatorio {
+    static void gerarRelatorioFinally(String caminhoEntrada, String caminhoSaida) {
+        BufferedReader leitor = null;
+        BufferedWriter escritor = null;
+        try {
+            leitor = new BufferedReader(new FileReader(caminhoEntrada));
+            escritor = new BufferedWriter(new FileWriter(caminhoSaida));
+
+            String linha;
+            boolean primeiraLinha= true;
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
