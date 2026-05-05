c3f1fcbc6da8363da6873f91591d7906a0b21840
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 09:28:48 -0300

    Alterando GerenciadorDeUsuarios.java, Aula-13

--- src/Aula13/GerenciadorDeUsuarios.java
+++ src/Aula13/GerenciadorDeUsuarios.java
@@ -3,7 +3,7 @@ package Aula13;
 import java.time.LocalDateTime;
 
 public class GerenciadorDeUsuarios {
-    public void cadastrar(String nome, LocalDateTime dataNascimento){
+    public void cadastrar(String nome, LocalDateTime dataNascimento) throws ValidacaotextualException, ValidacaoDataException{
         Usuario.validar(nome, dataNascimento);
     }
 }
