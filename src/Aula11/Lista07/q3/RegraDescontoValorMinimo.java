0ed48c5be80528778cb158b432578f7d30b27bc7
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 22:49:27 -0300

    Adicionando RegraDescontoValorMinimo.java da Questão-02, Aula-11

---
+++ src/Aula11/Lista07/q3/RegraDescontoValorMinimo.java
@@ -0,0 +1,18 @@
+package Aula11.Lista07.q3;
+
+public class RegraDescontoValorMinimo implements Regra {
+    @Override
+    public double getDesconto(Carrinho carrinho) {
+        double subtotal = 0;
+
+        for (Item item : carrinho.getItens()) {
+            subtotal += item.getPrecoUnitario() * item.getQuantidade();
+        }
+
+        if (subtotal > 300) {
+            return subtotal * 0.1;
+        }
+
+        return 0;
+    }
+}
