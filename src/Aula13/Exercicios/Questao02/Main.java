adabd2e10229b340905543d8bc51b9c82fb785dc
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 20:23:55 -0300

    Alterando a Main.java da Questão-02, Aula-13

--- src/Aula13/Exercicios/Questao02/Main.java
+++ src/Aula13/Exercicios/Questao02/Main.java
@@ -1,4 +1,20 @@
 package Aula13.Exercicios.Questao02;
 
+import static Aula13.Exercicios.Questao02.GeradorDeRelatorio.gerarRelatorioFinally;
+import static Aula13.Exercicios.Questao02.GeradorDeRelatorio.gerarRelatorioTWR;
+
 public class Main {
+    static void main(String[] args) {
+        String csvValido = "alunos.csv";
+        String csvInexistente = "fantasma.csv";
+
+        System.out.println("══════════════ Teste com arquivo válido ══════════════");
+        gerarRelatorioFinally(csvValido, "relatorio_A.txt");
+        gerarRelatorioTWR(csvValido, "relatorio_B.txt");
+
+        System.out.println("\n══════════════ Teste com arquivo inexistente ══════════");
+        gerarRelatorioFinally(csvInexistente, "relatorio_A_erro.txt");
+        gerarRelatorioTWR(csvInexistente, "relatorio_B_erro.txt");
+
+    }
 }
