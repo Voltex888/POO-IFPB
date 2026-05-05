cd19a9d88a113ae7e54dc5253f65bb75d8cc935a
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 09:25:44 -0300

    Alterando Usuario.java, Aula-13

--- src/Aula13/Usuario.java
+++ src/Aula13/Usuario.java
@@ -1,4 +1,10 @@
 package Aula13;
 
+import java.time.LocalDateTime;
+
 public class Usuario {
+    public static void validar(String nome, LocalDateTime dataNascimento) throws ValidacaotextualException, ValidacaoDataException{
+        if (nome == null) throw new ValidacaotextualException("O nome do usuario não pode ser nulo");
+        if (dataNascimento == null) throw new ValidacaoDataException("A data não pode ser nula");
+    }
 }
