5c0fe2950397291798357162b16c27899fc8c201
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 22:52:01 -0300

    Adicionando RegraDescontoSegundaUnidade.java, Aula-12

--- src/Aula12/RegraDescontoSegundaUnidade.java
+++ src/Aula12/RegraDescontoSegundaUnidade.java
@@ -1,4 +1,22 @@
 package Aula12;
 
-public class RegraDescontoSegundaUnidade {
+import java.util.*;
+
+public class RegraDescontoSegundaUnidade implements Regra {
+    @Override
+    public double getDesconto(Carrinho carrinho) {
+        List<Item> distintos = carrinho.getItens().stream().distinct().toList();
+
+        if (distintos.size() < 2) return 0;
+
+        Item maisBarato = distintos.get(0);
+
+        for (Item item : distintos) {
+            if (item.getPrecoUnitario() < maisBarato.getPrecoUnitario()) {
+                maisBarato = item;
+            }
+        }
+
+        return maisBarato.getPrecoUnitario() * 0.5;
+    }
 }
