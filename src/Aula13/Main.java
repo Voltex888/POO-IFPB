ef76a6deeba2efbac3eead82890eb165d1c8c2c0
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 10:18:29 -0300

    Adicionando Main.java, Aula-13

--- src/Aula13/Main.java
+++ src/Aula13/Main.java
@@ -1,12 +1,24 @@
 package Aula13;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() throws ValidacaoException{
         GerenciadorDeUsuarios g = new GerenciadorDeUsuarios();
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+
         try{
             g.cadastrar(null, null);
-        } catch (ValidacaoException e){
-          e.printStackTrace();
+        } catch (ValidacaotextualException | ValidacaoDataException e){
+          //e.printStackTrace();
+            System.out.println("Tipo de tratamento para erro");
+          throw new IllegalArgumentException();
+        } finally {
+            System.out.println("Finally executado");
+        }
+        if (sc != null){
+            sc.close();
         }
     }
 }
