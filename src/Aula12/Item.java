a17a43789ad5b758ec5bba119c9a828c8aa85a2e
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 22:51:21 -0300

    Adicionando Item.java, Aula-12

--- src/Aula12/Item.java
+++ src/Aula12/Item.java
@@ -1,4 +1,25 @@
 package Aula12;
 
 public class Item {
+    public Item(String nome, double precoUnitario, int quantidade) {
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    String nome;
+    double precoUnitario;
+    int quantidade;
 }
