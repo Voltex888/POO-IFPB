536175cb83b7b0e5098731b9bc555cadb2154876
Matheus <Matheuskkevin021@gmail.com>
2026-05-04 22:52:46 -0300

    Adicionando RegraFreteGratis.java, Aula-12

--- src/Aula12/RegraFreteGratis.java
+++ src/Aula12/RegraFreteGratis.java
@@ -1,4 +1,17 @@
 package Aula12;
 
-public class RegraFreteGratis {
+public class RegraFreteGratis implements Regra {
+    @Override
+    public double getDesconto(Carrinho carrinho) {
+        int quantidade = 0;
+
+        for (Item i : carrinho.getItens()) {
+            quantidade += i.quantidade;
+        }
+        if (quantidade > 5) {
+            return carrinho.getValorFrete();
+        }
+
+        return 0;
+    }
 }
