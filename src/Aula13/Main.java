1eca780a78c277307e7fab8ed4e082505edc2ab4
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 09:25:21 -0300

    Alterando Main.java, Aula-13

--- src/Aula13/Main.java
+++ src/Aula13/Main.java
@@ -1,4 +1,12 @@
 package Aula13;
 
 public class Main {
+    static void main() throws ValidacaoException{
+        GerenciadorDeUsuarios g = new GerenciadorDeUsuarios();
+        try{
+            g.cadastrar(null, null);
+        } catch (ValidacaoException e){
+          e.printStackTrace();
+        }
+    }
 }
