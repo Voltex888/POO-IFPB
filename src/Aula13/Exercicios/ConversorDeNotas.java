4890e906ffd01d38f93dda730918f9083a77e1cc
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 10:54:47 -0300

    Criando Construtor e métodos getters e setters em ConversorDeNotas.java de Exercicio-01, Aula-13

--- src/Aula13/Exercicios/ConversorDeNotas.java
+++ src/Aula13/Exercicios/ConversorDeNotas.java
@@ -1,4 +1,21 @@
 package Aula13.Exercicios;
 
 public class ConversorDeNotas {
+    private double nota;
+
+    public ConversorDeNotas() {
+    }
+
+    public ConversorDeNotas(double nota) {
+        this.nota = nota;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
 }
