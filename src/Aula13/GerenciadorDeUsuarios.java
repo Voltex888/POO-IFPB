22bb65606d1d7d00b6116393cc73ef406897e51b
Matheus <Matheuskkevin021@gmail.com>
2026-05-05 09:24:57 -0300

    Alterandi GerenciadorDeUsuarios.java, Aula-13

--- src/Aula13/GerenciadorDeUsuarios.java
+++ src/Aula13/GerenciadorDeUsuarios.java
@@ -1,4 +1,9 @@
 package Aula13;
 
+import java.time.LocalDateTime;
+
 public class GerenciadorDeUsuarios {
+    public void cadastrar(String nome, LocalDateTime dataNascimento){
+        Usuario.validar(nome, dataNascimento);
+    }
 }
