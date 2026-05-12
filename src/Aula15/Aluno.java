41a0603e9831d3d546c6fc47bfd27422fe400c51
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:22:24 -0300

    Criando método toString() de Aluno (javaCollections), Aula-15

--- src/Aula15/Aluno.java
+++ src/Aula15/Aluno.java
@@ -10,4 +10,9 @@ public class Aluno {
         this.idade = idade;
         this.nota = nota;
     }
+
+    @Override
+    public String toString() {
+        return nome + " - Com idade: " + idade + " - nota: " + nota;
+    }
 }
