531ab855cd3f8a3d2944f1e4452d9451be150165
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:37:59 -0300

    Criando Lista de alunos na MainCollections (javaCollections), Aula-15

---
+++ src/Aula15/MainCollections.java
@@ -0,0 +1,14 @@
+package Aula15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCollections {
+    static void main(String[] args) {
+        List<Aluno> alunos = new ArrayList<>();
+
+        alunos.add(new Aluno("Matheus", 20, 9.5));
+        alunos.add(new Aluno("Walmir", 19, 10));
+        alunos.add(new Aluno("Arthur", 24, 9.75));
+    }
+}
