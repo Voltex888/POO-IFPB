10c097115202272eecd948710636223e07413394
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:28:23 -0300

    Criando métodos Getters de Aluno (javaCollections), Aula-15

--- src/Aula15/Aluno.java
+++ src/Aula15/Aluno.java
@@ -11,6 +11,18 @@ public class Aluno {
         this.nota = nota;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
     @Override
     public String toString() {
         return nome + " - Com idade: " + idade + " - nota: " + nota;
