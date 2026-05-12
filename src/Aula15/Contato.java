a8028e76c4138784612a9eb92364427ee6eee024
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:27:41 -0300

    Criando Construtor de Contato (javaCollections), Aula-15

--- src/Aula15/Contato.java
+++ src/Aula15/Contato.java
@@ -4,4 +4,8 @@ public class Contato {
     private String nome;
     private String telefone;
 
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
 }
