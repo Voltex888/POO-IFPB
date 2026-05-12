73218533a06ea913ce03046f98538263c49eb3c9
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:44:51 -0300

    Criando o primeiro tipo de Collection em alunos (javaCollections), Aula-15

--- src/Aula15/MainCollections.java
+++ src/Aula15/MainCollections.java
@@ -10,5 +10,10 @@ public class MainCollections {
         alunos.add(new Aluno("Matheus", 20, 9.5));
         alunos.add(new Aluno("Walmir", 19, 10));
         alunos.add(new Aluno("Arthur", 24, 9.75));
+
+        System.out.println("LISTA DE ALUNOS");
+        for (Aluno a : alunos) {
+            System.out.println(a.toString());
+        }
     }
 }
