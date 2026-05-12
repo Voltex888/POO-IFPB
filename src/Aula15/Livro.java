91b12ada66736be053717c642b552d751d985d1b
Matheus <Matheuskkevin021@gmail.com>
2026-05-12 10:31:41 -0300

    Criando construtor de Livro (javaCollections), Aula-15

--- src/Aula15/Livro.java
+++ src/Aula15/Livro.java
@@ -3,4 +3,9 @@ package Aula15;
 public class Livro {
     private String titulo;
     private String autor;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 }
